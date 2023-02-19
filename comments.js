186875a2c2b2aede03025f957d735d940413357c
Sagar Saluja <sagar.saluja@onsolve.com>
2023-02-19 15:44:05 +0530

    basics of playing audio with js

---
+++ comments.js
@@ -0,0 +1,6 @@
+//Many browsers don't allow you to analyze your audio file just like pixel data for security reasons
+//so we'll convert our audio into a base 64 string first => but this string can be very long.
+//just use an audio to base 64 string converter online.
+
+//second way is to create a file input html element
+  
